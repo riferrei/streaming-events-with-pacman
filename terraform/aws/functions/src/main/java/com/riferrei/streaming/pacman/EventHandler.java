ddd4a1b69e250e8c0fb74be75f5056cf2be1da43
riferrei <riferrei@riferrei.com>
2021-11-21 16:08:18 -0500

    Fixed dependencies vulnerabilities

--- terraform/aws/functions/src/main/java/com/riferrei/streaming/pacman/EventHandler.java
+++ terraform/aws/functions/src/main/java/com/riferrei/streaming/pacman/EventHandler.java
@@ -58,62 +58,6 @@ public class EventHandler implements RequestHandler<APIGatewayProxyRequestEvent,
 
     }
 
-    /*
-    public Map<String, Object> handleRequest(final Map<String, Object> request, final Context context) {
-        
-        Map<String, Object> response = new HashMap<>();
-
-        @SuppressWarnings("unchecked")
-        Map<String, Object> requestHeaders =
-            (Map<String, Object>) request.get(HEADERS_KEY);
-
-        if (requestHeaders.containsKey(ORIGIN_KEY)) {
-
-            String origin = (String) requestHeaders.get(ORIGIN_KEY);
-
-            if (origin.equals(ORIGIN_ALLOWED)) {
-
-                if (request.containsKey(QUERY_PARAMS_KEY) && request.containsKey(BODY_KEY)) {
-
-                    String event = (String) request.get(BODY_KEY);
-                    @SuppressWarnings("unchecked")
-                    Map<String, String> queryParams =
-                        (Map<String, String>) request.get(QUERY_PARAMS_KEY);
-
-                    if (event != null && queryParams != null) {
-
-                        String topic = queryParams.get(TOPIC_KEY);
-                        String user = extractUserFromEvent(event);
-
-                        ProducerRecord<String, String> record = new ProducerRecord<>(topic, user, event);
-                        producer.send(record, (metadata, exception) -> {
-                            StringBuilder message = new StringBuilder();
-                            message.append("Event sent successfully to topic '");
-                            message.append(metadata.topic()).append("' on the ");
-                            message.append("partition ").append(metadata.partition());
-                            response.put(BODY_KEY, message.toString());
-                        });
-                        producer.flush();
-
-                        Map<String, Object> responseHeaders = new HashMap<>();
-                        responseHeaders.put("Access-Control-Allow-Headers", "*");
-                        responseHeaders.put("Access-Control-Allow-Methods", POST_METHOD);
-                        responseHeaders.put("Access-Control-Allow-Origin", ORIGIN_ALLOWED);
-                        response.put(HEADERS_KEY, responseHeaders);
-
-                    }
-        
-                }
-                
-            }
-            
-        }
-        
-        return response;
-
-    }
-    */
-
     private String extractUserFromEvent(String payload) {
         JsonElement root = JsonParser.parseString(payload);
         return root.getAsJsonObject().get("user").getAsString();
